dd4cbbcb357c1b1219505fc8365f319d9c7cc2af
KuoKongQingYun <609898048@qq.com>
2015-01-04 23:46:45 +0800

    some fix
    some fix

--- www/js/taking_photo_event_handlers.js
+++ www/js/taking_photo_event_handlers.js
@@ -4,9 +4,16 @@
         if (event.success === true) {
             var imagesrc = intel.xdk.camera.getPictureURL(event.filename);
             $("#photo").attr("src", imagesrc);
-            intel.xdk.file.uploadToServer(imagesrc, "http://flashandshare.linzhihao.cn/uploadImage.php", "", "image/jpeg", "updateUploadProgress");
-            alert("");
-            xmlHttpRequest = createXmlHttpRequest();
+            
+            
+            var options = new FileUploadOptions();
+            options.fileKey = "file";
+            options.fileName = imagesrc.substr(fileURL.lastIndexOf('/') + 1);
+            options.mimeType = "image/jpeg";
+            var ft = new FileTransfer();
+            ft.upload(imagesrc,encodeURI("http://flashandshare.linzhihao.cn/index.php"), upload_success, upload_fail, options);
+            //intel.xdk.file.uploadToServer(imagesrc,"http://flashandshare.linzhihao.cn/uploadImage.php", "", "image/jpeg", "updateUploadProgress");
+            //xmlHttpRequest = createXmlHttpRequest();
         } else {
             if (event.message !== undefined) {
                 alert(event.message);
--- www/js/uploading_event_handlers.js
+++ www/js/uploading_event_handlers.js
@@ -1,28 +1,15 @@
 (function() {
     "use strict";
-    function updateUploadProgress(bytesSent, totalBytes) {
-        if (totalBytes > 0) currentProgress = (bytesSent / totalBytes) * 100;
-        document.getElementById("progress").innerHTML = currentProgress + "%";
+
+    function upload_success(r) {
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        console.log("Sent = " + r.bytesSent);
     }
-    
-    function uploadBusy(evt) {
-        alert("Sorry, a file is already being uploaded");
+    function upload_fail(error) {
+        alert("An error has occurred: Code = " + error.code);
+        console.log("upload error source " + error.source);
+        console.log("upload error target " + error.target);
     }
 
-    function uploadComplete(evt) {
-        alert("123");
-        if (evt.success === true) {
-            alert("File " + evt.localURL + " was uploaded");
-            $("#URL").html(evt.localURL);
-        } else {
-            alert("Error uploading file " + evt.message);
-        }
-    }
-
-    function uploadCancelled(evt) {
-        alert("File upload was cancelled " + evt.localURL);
-    }
-    document.addEventListener("intel.xdk.file.upload.busy", uploadBusy);
-    document.addEventListener("intel.xdk.file.upload", uploadComplete);
-    document.addEventListener("intel.xdk.file.upload.cancel", uploadCancelled);
 })();
