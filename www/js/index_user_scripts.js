667db64da24ea04de3172235f4a208b1bc0b13b5
KuoKongQingYun <609898048@qq.com>
2015-01-05 12:09:34 +0800

    beta1
    beta1

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,28 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  咔嚓 */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+        intel.xdk.camera.takePicture(80,false,"jpg");
+        /* your code goes here */ 
+    });
+    
+        /* button  复制到剪切板 */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+        intel.xdk.device.copyToClipboard($["#URL"].html());
+        alert("已复制到剪切板");
+        /* your code goes here */ 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
--- www/js/taking_photo_event_handlers.js
+++ www/js/taking_photo_event_handlers.js
@@ -1,19 +1,34 @@
 (function() {
     "use strict";
+    function upload_success(r) {
+        $("#URL").html(r.response);
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        console.log("Sent = " + r.bytesSent);
+    }
+    function upload_fail(error) {
+        alert("An error has occurred: Code = " + error.code);
+        console.log("upload error source " + error.source);
+        console.log("upload error target " + error.target);
+    }
+    
     function onTakingPhoto(event) {
         if (event.success === true) {
             var imagesrc = intel.xdk.camera.getPictureURL(event.filename);
             $("#photo").attr("src", imagesrc);
-            
-            
+
             var options = new FileUploadOptions();
             options.fileKey = "file";
-            options.fileName = imagesrc.substr(fileURL.lastIndexOf('/') + 1);
+            options.fileName = imagesrc.substr(imagesrc.lastIndexOf('/') + 1);
             options.mimeType = "image/jpeg";
-            var ft = new FileTransfer();
-            ft.upload(imagesrc,encodeURI("http://flashandshare.linzhihao.cn/index.php"), upload_success, upload_fail, options);
-            //intel.xdk.file.uploadToServer(imagesrc,"http://flashandshare.linzhihao.cn/uploadImage.php", "", "image/jpeg", "updateUploadProgress");
-            //xmlHttpRequest = createXmlHttpRequest();
+            
+            try {
+                var ft = new FileTransfer();
+            } catch(e) {
+                alert(e.message);
+            }
+            ft.upload(imagesrc, encodeURI("http://flashandshare.linzhihao.cn/index.php"), upload_success, upload_fail, options);
+            
         } else {
             if (event.message !== undefined) {
                 alert(event.message);
